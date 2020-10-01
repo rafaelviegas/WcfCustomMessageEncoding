89444f8018071c44f67b4a89a88df0bb9b082644
Rafael Viegas <rafael.viegas@arpagus.com.br>
2020-10-01 10:25:33 -0300

    chore: added the encoder factory

---
+++ src/WcfCustomMessageEncoding/CustomTextMessageEncoderFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.ServiceModel.Channels;
+
+namespace WcfCustomMessageEncoding
+{
+    public class CustomTextMessageEncoderFactory : MessageEncoderFactory
+    {
+        private readonly MessageEncoder _encoder;
+        private readonly MessageVersion _version;
+
+        internal CustomTextMessageEncoderFactory(string mediaType, string charSet, MessageVersion version)
+        {
+            _version = version;
+            MediaType = mediaType;
+            CharSet = charSet;
+            _encoder = new CustomTextMessageEncoder(this);
+        }
+
+        public override MessageEncoder Encoder
+        {
+            get
+            {
+                return _encoder;
+            }
+        }
+
+        public override MessageVersion MessageVersion
+        {
+            get
+            {
+                return _version;
+            }
+        }
+
+        internal string MediaType { get; }
+
+        internal string CharSet { get; }
+    }
+}
